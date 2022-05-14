2437536cb304941e7b7e27eb080a084519618021
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-05-14 15:42:18 +0900

    login 기능 구현 및 token 생성

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const config = require('./config/key');
 const {User} = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.use(bodyParser.json() );
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -28,6 +31,45 @@ app.post('/register',(req, res) => {
             success : true
         })
     })
+
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 데이터베이스에 있는지 찾는다
+    User.findOne({ email : req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+    // 요청한 이메일이 데이터베이스에 있으면 비밀번호 같은지도 확인한다 
+    
+    user.comparePassword(req.body.password,(err, isMatch) => {
+        if(!isMatch)
+            return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+        
+    // 비밀번호까지 같다면 토큰을 생성한다 
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지 등등 
+            res.cookie("x_auth", user.token)
+            .status(200)
+            .json({loginSuccess:true, userId : user._id})
+
+
+        })
+
+    })
+
+    })
+
+
+
+
+
+
+
 
 })
 
